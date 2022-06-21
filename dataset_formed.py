5c9b7ed5084679bf4299a36c494a785e0bdc03d9
TimeXZone <36071661+TimeXZone@users.noreply.github.com>
2022-06-21 19:50:53 +0800

    Update dataset_formed.py
    adding random location

--- dataset_formed.py
+++ dataset_formed.py
@@ -1,11 +1,23 @@
 import copy
 import os
+import random
 from os import path
 import cv2 as cv2
+import numpy as np
+
+
+def check_iou(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+    # 判断如果没有相交直接返回 0 相交返回1
+    if ax1 >= bx2 or ax2 <= bx1 or ay1 >= by2 or ay2 <= by1:
+        return 0
+    else:
+        return 1
 
 
 class dataset_formed:
     def __init__(self, dataset_path, dataset_image_folder_name):
+        self.dataset_image_part_info = None
+        self.dataset_images_shape = None
         self.inserted_images_with_annotations = None
         self.dataset_images_info_with_file_name = None
         self.dataset_path = dataset_path
@@ -51,12 +63,130 @@ class dataset_formed:
             annotation_file_name = annotation_file_name + '.' + 'txt'
             cv2.imwrite(path.join(imgs_abs_path + '/' + img_file_name), image)
             with open(path.join(annotation_abs_path + '/' + annotation_file_name), "w", encoding="utf-8") as f:
+                for annotation in annotations:
                     f.write(
-                        str(annotations['x0']) + ',' + str(annotations['y0']) + ',' + str(annotations['x1']) + ',' + str(
-                            annotations['y1']) + ',' + str(annotations['x2']) + ',' + str(annotations['y2']) + ',' + str(
-                            annotations['x3']) + ',' + str(annotations['y3']) + ',' + str(annotations['result']) + '\n')
+                        str(annotation['x0']) + ',' + str(annotation['y0']) + ',' + str(annotation['x1']) + ',' + str(
+                            annotation['y1']) + ',' + str(annotation['x2']) + ',' + str(annotation['y2']) + ',' + str(
+                            annotation['x3']) + ',' + str(annotation['y3']) + ',' + str(annotation['result']) + '\n')
 
             i = i + 1
+
+    def load_dataset_images_info(self):
+        # 加载数据集的图片信息
+        self.dataset_image_part_info = []
+        for dataset_image_info in self.dataset_images_info_with_file_name:
+            f = path.join(dataset_image_info['file_name'] + '.' + dataset_image_info['file_format'])
+            processing_image_path_now = path.join(self.dataset_imgs_path_now + '/' + f)
+            processing_image_now = cv2.imread(processing_image_path_now)
+            processing_image_now_shape = processing_image_now.shape
+            (y, x, a) = processing_image_now_shape
+            self.dataset_images_shape = (y, x, a)
+            for annotations in dataset_image_info['annotations']:
+                # print(annotations)
+                temp_image_origin = processing_image_now[int(annotations['y0']):int(annotations['y2']),
+                                    # 已完成所需要训练部分摘取
+                                    int(annotations['x0']):int(annotations['x2'])]
+                position = [int(annotations['x0']), int(annotations['y0'])]
+                lenth = [int(annotations['x2']) - int(annotations['x0']),
+                         int(annotations['y2']) - int(annotations['y0'])]
+                (lena, wida) = lenth
+                image_part_info = {'image': temp_image_origin, 'annotations': annotations, 'position': position,
+                                   'lenth': lenth, 'square': abs(lena) * abs(wida)}
+                self.dataset_image_part_info.append(image_part_info)
+        return self.dataset_image_part_info
+
+    def make_random_insert(self, other_image_path_):
+
+        # print(temp_img_part_info)
+        # print(self.dataset_images_info_with_file_name)
+        file = os.listdir(other_image_path_)
+        # print(file)
+        insert_images = []
+        inserted_images_with_annotations = []
+        for insert_image_name in file:
+            insert_image = cv2.imread(path.join(other_image_path_ + '/' + insert_image_name))
+            insert_images.append(insert_image)
+            # insert_image = cv2.resize(insert_image, (1280, 720), interpolation=cv2.INTER_LINEAR)
+            # insert_image_shape = insert_image.shape
+            # print(insert_image_shape)
+        # print(insert_images)
+        # 将dataset_image_info_with_file_name中的图片信息提取出来并打包成为只有待增强图像的信息
+        self.load_dataset_images_info()
+        # print(self.dataset_image_part_info)
+        temp_imgs_part_info = sorted(self.dataset_image_part_info, key=lambda x: x['square'],
+                                     reverse=True)  # 按照图片的面积进行排序
+        # print(temp_imgs_part_info)
+
+        for insert_image in insert_images:
+            insert_image = cv2.resize(insert_image, (self.dataset_images_shape[1], self.dataset_images_shape[0]),
+                                      interpolation=cv2.INTER_LINEAR)
+            new_insert_image = copy.deepcopy(insert_image)  # 图片拷贝
+            (y, x, a) = self.dataset_images_shape
+            count = 5
+            overlay_map = np.zeros((x, y), bool)  # 创建一个覆盖图，0为可用区域1为不可用区域
+            annotations = []  # 保存增强后的标注信息列表
+            for temp_img_part_info in temp_imgs_part_info:  # 将图片插入到insert_image中
+                # print(temp_img_part_info)
+                (x0, y0) = temp_img_part_info['position']
+                (x1, y1) = temp_img_part_info['lenth']
+
+                # print(y0, x0, y1, x1)
+                # try:
+                if count == 0:  # count=0时保存前面的图像信息开辟新图像
+                    inserted_images_with_annotations.append({'image': new_insert_image, 'annotations': annotations})
+                    new_insert_image = copy.deepcopy(insert_image)  # 图片拷贝
+                    (y, x, a) = self.dataset_images_shape
+                    overlay_map = np.zeros((x, y), bool)  # 创建一个覆盖图，0为可用区域1为不可用区域
+                    annotations = []  # 保存增强后的标注信息列表
+                    count = 5  # 同一图中至多5次插入图像
+                sgn = 0
+                for i in range(0, 5):  # 尝试5次找位置插入图像
+                    x0 = random.randint(0, x - x1-1)
+                    y0 = random.randint(0, y - y1-1)
+                    if overlay_map[x0, y0] == False and overlay_map[x0 + x1, y0 + y1] == False and overlay_map[
+                        x0, y0 + y1] == False and overlay_map[x0 + x1, y0] == False and count > 0:
+                        new_insert_image[y0:y0 + y1, x0:x0 + x1] = temp_img_part_info['image']
+                        overlay_map[x0:x0 + x1, y0:y0 + y1] = True
+                        annotation = temp_img_part_info['annotations']
+                        annotation['x0'] = x0
+                        annotation['y0'] = y0
+                        annotation['x1'] = x0 + x1
+                        annotation['y1'] = y0
+                        annotation['x2'] = x0 + x1
+                        annotation['y2'] = y0 + y1
+                        annotation['x3'] = x0
+                        annotation['y3'] = y0 + y1
+                        annotations.append(annotation)
+                        sgn = 1  # 插入成功
+                        break
+                if sgn == 1:
+                    count = count - 1  # 继续插入下一张图片
+                    continue
+                if sgn == 0:  # 保存前面的图像信息开辟新图将插不进的这张图插进去
+                    inserted_images_with_annotations.append({'image': new_insert_image, 'annotations': annotations})
+                    new_insert_image = copy.deepcopy(insert_image)  # 重置新图像拷贝
+                    annotations = []  # 重置标注信息
+                    overlay_map = np.zeros((x, y), bool)  # 重置覆盖图
+                    count = 5  # 重置插入次数
+                    # 将插不进的图片插到重置后的覆盖图里
+                    new_insert_image[y0:y0 + y1, x0:x0 + x1] = temp_img_part_info['image']
+                    overlay_map[x0:x0 + x1, y0:y0 + y1] = True
+                    annotation = temp_img_part_info['annotations']
+                    annotation['x0'] = x0
+                    annotation['y0'] = y0
+                    annotation['x1'] = x0 + x1
+                    annotation['y1'] = y0
+                    annotation['x2'] = x0 + x1
+                    annotation['y2'] = y0 + y1
+                    annotation['x3'] = x0
+                    annotation['y3'] = y0 + y1
+                    annotations.append(annotation)
+                    count = count - 1
+                    continue
+        inserted_images_with_annotations.append(
+            {'image': new_insert_image, 'annotations': annotations})  # 将最后一张图片保存到队列中
+        self.inserted_images_with_annotations = inserted_images_with_annotations
+        return self.inserted_images_with_annotations
 
     def make_insert(self, other_image_path_):
         file = os.listdir(other_image_path_)
@@ -74,7 +204,6 @@ class dataset_formed:
             f = path.join(dataset_image_info['file_name'] + '.' + dataset_image_info['file_format'])
             processing_image_path_now = path.join(self.dataset_imgs_path_now + '/' + f)
             processing_image_now = cv2.imread(processing_image_path_now)
-
             processing_image_now_shape = processing_image_now.shape
             (y, x, a) = processing_image_now_shape
             for insert_image in insert_images:
@@ -95,6 +224,7 @@ class dataset_formed:
                     # cv2.waitKey()
                     # cv2.destroyAllWindows()
             # print(processing_image_now_shape)
+            # print()
         self.inserted_images_with_annotations = inserted_images_with_annotations
         return inserted_images_with_annotations
 
@@ -139,6 +269,7 @@ out_put_path1 = "toy_dataset_augmented"
 
 test = dataset_formed(dataset_path1, dataset_image_path1)
 test.info_open()
-augmented_images = test.make_insert(other_image_path)
+# augmented_images = test.make_insert(other_image_path)
+test.make_random_insert(other_image_path)
 test.save_result(out_put_path1)
 # print(augmented_images)
